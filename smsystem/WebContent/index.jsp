13e9a08265f22e0ed736662242f223f1a8cd1794
computer <computer@appsquad>
2017-01-05 16:41:48 +0530

    #

--- smsystem/WebContent/index.jsp
+++ smsystem/WebContent/index.jsp
@@ -27,11 +27,9 @@
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="assets/bootstrap/js/ie-emulation-modes-warning.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <script type="text/javascript">
+      console.log('ffff');
+    </script>
   </head>
 
   <body>
@@ -101,5 +99,6 @@
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="assets/bootstrap/js/ie10-viewport-bug-workaround.js"></script>
     <script src="assets/custom/js/register.js"></script>
+    <script src="assets/custom/js/registerdemo.js"></script>
   </body>
 </html>
---
+++ smsystem/src/com/smsystem/UserRegisterDemo.java
@@ -0,0 +1,40 @@
+package com.smsystem;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserRegisterDemo
+ */
+@WebServlet("/UserRegisterDemo")
+public class UserRegisterDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserRegisterDemo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	    String name= (String)request.getParameter("name");
+	    String address= (String)request.getParameter("address");
+	    String email= (String)request.getParameter("email");
+	    String pwd= (String)request.getParameter("pwd");
+	    Integer age=Integer.parseInt(request.getParameter("age"));
+			
+	    System.out.println("AJAX CALL");
+	    response.getWriter().write("Hi,Your registration successfull with "+email+" email address!!");
+		}
+
+}
